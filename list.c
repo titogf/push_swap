3c3895cd07bc8b582b996ef6aa07b096bd137d4d
titogf <gonchogfa@gmail.com>
2022-12-12 14:49:17 +0100

    have to change the list

--- list.c
+++ list.c
@@ -3,15 +3,17 @@
 
 t_list	ft_create_list(t_nb *nb)
 {
-	t_list	list;
+	t_list	*list;
 	int	i;
 
 	i = -1;
-	list.content = 0;
+	list = malloc (sizeof (t_list));
+	list->content = 0;
 	while (++i < nb->numbers)
 	{
-		list.content = nb->n[i];
-		list = list.next;
+		list->content = nb->n[i];
+		list = list->next;
 	}
-	list.content = NULL;
+	list->content = NULL;
+	return (*list);
 }
