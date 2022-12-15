1d0ccacd28c2f8a22e1dc73cfe83c4d759325b4b
titogf <gonchogfa@gmail.com>
2022-12-15 13:57:09 +0100

    list incorrect

--- list.c
+++ list.c
@@ -6,9 +6,9 @@ t_list	ft_create_list(t_nb *nb)
 	t_list	*list;
 	int	i;
 
-	i = -1;
 	list = malloc (sizeof (t_list));
-	list->content = 0;
+	list->next = malloc (sizeof (t_list));
+	i = -1;
 	while (++i < nb->numbers)
 	{
 		list->content = nb->n[i];
