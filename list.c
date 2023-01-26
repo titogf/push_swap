3da4fec62ec92e1fdb757739f0980bda1b5eba88
GFA <98211123+titogf@users.noreply.github.com>
2023-01-26 13:45:58 +0100

    struct list changed

--- list.c
+++ list.c
@@ -6,7 +6,7 @@
 /*   By: gfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 17:28:17 by gfernand          #+#    #+#             */
-/*   Updated: 2023/01/26 12:19:19 by gfernand         ###   ########.fr       */
+/*   Updated: 2023/01/26 12:33:07 by gfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,13 @@ t_lst	ft_create_list(t_nb *nb)
 	dir = &list;
 	printf("DIR = %p\n", dir);
 	i = -1;
-	printf("LIST:\n");
 	while (++i < nb->numbers && list != NULL)
 	{
 		list->content = nb->n[i];
 		if (i < nb->numbers)
 			list->next = (t_lst *)ft_lstnew(nb->n[i]);
-		printf(" %d\n", list->content);
+		printf("List-> %d\n", list->content);
 		list = list->next;
 	}
-	list = dir;
-	printf(" %d\n", list->content);
 	return (*list);
 }
