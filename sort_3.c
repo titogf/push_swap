2a1db0df45e498e7ee571cf93dc5bc7cab760dbb
GFA <98211123+titogf@users.noreply.github.com>
2023-03-06 20:09:03 +0100

    comun functions

--- sort_3.c
+++ sort_3.c
@@ -6,7 +6,7 @@
 /*   By: gfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 15:43:28 by gfernand          #+#    #+#             */
-/*   Updated: 2023/03/06 18:50:26 by gfernand         ###   ########.fr       */
+/*   Updated: 2023/03/06 20:08:31 by gfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,69 @@
 
 void	ft_ss(t_stack *stack)
 {
-	if (!(stack->a || stack->b))
+	t_lst	*list_a;
+	t_lst	*list_b;
+	int		n;
+
+	list_a = stack->a;
+	n = -1;
+	while (list_a && ++n < 2)
+		list_a = list_a->next;
+	if (n < 2)
 		return ;
-	else
-	{
-		ft_sa(stack, 0);
-		ft_sb(stack, 0);
-		write (1, "ss\n", 3);
-	}
+	list_b = stack->b;
+	n = -1;
+	while (list_b && ++n < 2)
+		list_b = list_b->next;
+	if (n < 2)
+		return ;
+	ft_sa(stack, 0);
+	ft_sb(stack, 0);
+	write (1, "ss\n", 3);
 }
 
 void	ft_rr(t_stack *stack)
 {
-	if (!(stack->a || stack->b))
+	t_lst	*list_a;
+	t_lst	*list_b;
+	int		n;
+
+	list_a = stack->a;
+	n = -1;
+	while (list_a && ++n < 2)
+		list_a = list_a->next;
+	if (n < 2)
 		return ;
-	else
-	{
-		ft_ra(stack, 0);
-		ft_rb(stack, 0);
-		write (1, "rr\n", 3);
-	}
+	list_b = stack->b;
+	n = -1;
+	while (list_b && ++n < 2)
+		list_b = list_b->next;
+	if (n < 2)
+		return ;
+	ft_ra(stack, 0);
+	ft_rb(stack, 0);
+	write (1, "rr\n", 3);
 }
 
 void	ft_rrr(t_stack *stack)
 {
-	if (!(stack->a || stack->b))
+	t_lst	*list_a;
+	t_lst	*list_b;
+	int		n;
+
+	list_a = stack->a;
+	n = -1;
+	while (list_a && ++n < 2)
+		list_a = list_a->next;
+	if (n < 2)
 		return ;
-	else
-	{
-		ft_rra(stack, 0);
-		ft_rrb(stack, 0);
-		write (1, "rrr\n", 4);
-	}
+	list_b = stack->b;
+	n = -1;
+	while (list_b && ++n < 2)
+		list_b = list_b->next;
+	if (n < 2)
+		return ;
+	ft_rra(stack, 0);
+	ft_rrb(stack, 0);
+	write (1, "rrr\n", 4);
 }
